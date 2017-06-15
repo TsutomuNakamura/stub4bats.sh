823c6f69197190e0eb8acf743d8f0820efbe3864
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2017-06-15 12:54:50 +0900

    Change methods to store parameters

--- test/__stub_clean.test.sh
+++ test/__stub_clean.test.sh
@@ -10,9 +10,8 @@ STUB_INDEX=("uname=0" "top=1")
 STUB_0_CALLS=("<none>" "-r" "-r -a")
 STUB_1_CALLS=("-h")
 __stub_clean "uname"
-assert 'echo ${STUB_INDEX[@]}' "top=1"
-assert 'echo ${STUB_INDEX[0]}' "top=1"
-assert 'echo ${STUB_0_CALLS[@]}' ""
+
+assert ''
 
 
 # End of tests.
